12cb97a3f3cb561ceae6f31a6176c0d00aea22a0
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-02-17 18:11:39 -0800

    added questions with functionality to node

--- app.js
+++ app.js
@@ -17,37 +17,75 @@ const promptUser = () => {
         {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a Valid answer');
+                return false;
+            }
+            }
         },
+        {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+        },       
         {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+                return true;
+            } else {
+                return false;
+            }
+        }
         }
     ]);
 };
 
 const promptProject = portfolioData => {
 
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
-    }
-
     console.log(`
   =================
   Add a New Project
   =================
   `);
+
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+    }
+
     return inquirer.prompt([
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a Valid answer');
+                return false;
+            }
+            }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a Valid answer');
+                return false;
+            }
+            }
       },
       {
         type: 'checkbox',
@@ -58,7 +96,15 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a Valid answer');
+                return false;
+            }
+            }
       },
       {
         type: 'confirm',
@@ -72,20 +118,20 @@ const promptProject = portfolioData => {
         message: 'Would you like to enter another project?',
         default: false
       },
-    ]);
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
   };
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
       console.log(portfolioData);
-  })
-  .then(projectData => {
-      portfolioData.projects.push(projectData);
-      if (projectData.confirmAddProject) {
-          return promptProject(portfolioData);
-      } else {
-          return portfolioData;
-      }
   });
 
 // const fs = require('fs');
