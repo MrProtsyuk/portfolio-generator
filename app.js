57d485d308807238e7ac9b9dad3431a6e4e83d52
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-02-17 17:30:54 -0800

    ability to ask for name with node

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer')
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(ema, github);
 
-const [ema, github] = profileDataArgs;
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// })
 
-fs.writeFile('index.html', generatePage(ema, github), err => {
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
