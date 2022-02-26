793eaa43c629ad45d2eef6abfc78caadfae3e31c
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-02-26 13:51:44 -0800

    finished site

---
+++ utils/generate-site.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve ({
+                ok: true,
+                message: "File created."
+            });
+        });
+    });
+};
+
+const copyFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve ({
+                ok: true,
+                message: "Style Copied over."
+            });
+        });
+    });
+};
+
+module.exports = { writeFile, copyFile };
